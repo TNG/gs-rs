69d84cc7da25957966e1bb5464b8c2e299df82ae
Daniel Pape <daniel.pape@tngtech.com>
2020-03-11 09:39:28 +0100

    Linear Solvers:
    * Solver trait for arbitrary dimensions
    * Implement CholeskySolver
    * Implement LUSolver

---
+++ src/solver/lu/mod.rs
@@ -0,0 +1,94 @@
+#![allow(non_snake_case)]
+
+use nalgebra::{LU, DMatrix};
+use crate::solver::Solver;
+
+pub struct LUSolver;
+
+impl Solver for LUSolver {
+    fn solve(b: &[f64], H: &[f64]) -> Result<Vec<f64>, String> {
+        let dim = b.len();
+
+        if H.len() != dim * dim {
+            return Err(format!("Incompatible dimensions! The length of H is not the length of b squared: H.len() = {:?}; b.len() = {:?}", H.len(), dim));
+        }
+
+        let matrix_H = DMatrix::from_vec(dim, dim, H.to_vec());
+        let vector_b = DMatrix::from_vec(dim, 1, b.to_vec());
+
+        let lu_decomposition = LU::new(matrix_H);
+
+        match lu_decomposition.solve(&vector_b) {
+            None => Err(format!("H is not invertible: H = {:?}", H)),
+            Some(lu) => Ok(lu.data.into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use log::LevelFilter;
+
+    use crate::solver::lu::{LUSolver};
+    use crate::solver::Solver;
+
+    fn init() {
+        let _ = env_logger::builder()
+            .is_test(true)
+            .filter_level(LevelFilter::Debug)
+            .try_init();
+    }
+
+    #[test]
+    fn solver_invertible_test() {
+        init();
+        #[allow(non_snake_case)]
+        let invertible_H = vec![ 2.0, -1.0, 2.0,  // transposed H is displayed
+                                -1.0, 2.0, -1.0,
+                                 0.0, -1.0, 2.0];
+        let b = vec![6.0, 6.0, 6.0];
+        let solve_output = LUSolver::solve(&b, &invertible_H);
+        let x = match solve_output {
+            Ok(sol) => sol,
+            Err(str) => panic!(str)
+        };
+        info!("H = {:?}; b = {:?}  |  x = {:?}", invertible_H, b, x);
+        assert!(relative_eq!(x[0], 6.0, epsilon = 1e-10));
+        assert!(relative_eq!(x[1], 6.0, epsilon = 1e-10));
+        assert!(relative_eq!(x[2], 0.0, epsilon = 1e-10));
+    }
+
+    #[test]
+    #[should_panic]
+    fn solver_not_invertible_test() {
+        init();
+        #[allow(non_snake_case)]
+            let not_invertible_H = vec![0.0,  2.0, -1.0,  // transposed H is displayed
+                                        3.0, -2.0,  1.0,
+                                        3.0,  2.0, -1.0];
+        let b = vec![6.0, 6.0, 6.0];
+        let solve_output = LUSolver::solve(&b, &not_invertible_H);
+        let x = match solve_output {
+            Ok(sol) => sol,
+            Err(str) => panic!(str)
+        };
+        info!("TEST FAILED! The solver returned {:?} for not invertible H = {:?}", x, not_invertible_H);
+    }
+
+    #[test]
+    #[should_panic]
+    fn solver_incompatible_dimension_test() {
+        init();
+        #[allow(non_snake_case)]
+        let positive_definite_H = vec![ 2.0, -1.0,  0.0,  // transposed H is displayed
+                                       -1.0,  2.0, -1.0,
+                                        0.0, -1.0,  2.0];
+        let b = vec![6.0, 6.0, 6.0, 6.0];
+        let solve_output = LUSolver::solve(&b, &positive_definite_H);
+        let x = match solve_output {
+            Ok(sol) => sol,
+            Err(str) => panic!(str)
+        };
+        info!("TEST FAILED! The solver returned {:?} for incompatible dimensions: H = {:?}; b = {:?}", x, positive_definite_H, b);
+    }
+}
