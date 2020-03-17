b4c278a7b5887d0392d2c060df71a143b885cb54
Samuel <samuel.valenzuela@tngtech.com>
2020-03-17 13:19:01 +0100

    Interior mutability for variables:
    * Refine variable interfaces
    * Add TODOs and extra file for unclear points that should be discussed in the near future

--- src/factorgraph/variable/landmark_variable_2d.rs
+++ src/factorgraph/variable/landmark_variable_2d.rs
@@ -1,1 +1,68 @@
-// TODO implement after finishing PoseVariable2D
+use uuid::Uuid;
+use crate::factorgraph::variable::{Variable, VariableType};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+#[derive(Debug)]
+pub struct LandmarkVariable2D {
+    id: Uuid,
+    /// [x, y]
+    content: Rc<RefCell<[f64; 2]>>,
+}
+
+impl LandmarkVariable2D {
+    pub fn from_position(x: f64, y: f64) -> Self {
+        LandmarkVariable2D {
+            id: Uuid::new_v4(),
+            content: Rc::new(RefCell::new([x, y])),
+        }
+    }
+
+    /// method for testing how to edit the content
+    /// TODO create more appropriate editing interface
+    pub fn set_content(&self, x: f64, y: f64) {
+        *self.content.borrow_mut() = [x, y];
+    }
+}
+
+impl Variable<'_> for LandmarkVariable2D {
+    fn get_id(&self) -> Uuid { self.id }
+
+    fn get_type(&self) -> VariableType { VariableType::Landmark2D }
+
+    fn get_content(&self) -> Vec<f64> { (*self.content.borrow_mut()).to_vec() }
+}
+
+#[cfg(test)]
+mod tests {
+    use log::LevelFilter;
+    use super::*;
+
+    fn init() {
+        let _ = env_logger::builder()
+            .is_test(true)
+            .filter_level(LevelFilter::Debug)
+            .try_init();
+    }
+
+    #[test]
+    fn test_from_position() {
+        init();
+
+        let test_variable = LandmarkVariable2D::from_position(3.0, 5.0);
+        info!("{:?}", &test_variable);
+        assert_eq!(test_variable.get_content(), &[3.0, 5.0]);
+        assert_eq!(test_variable.get_type(), VariableType::Landmark2D);
+    }
+
+    #[test]
+    fn test_set_content() {
+        init();
+
+        let test_variable = LandmarkVariable2D::from_position(1.0, 1.0);
+        info!("{:?}", &test_variable);
+        assert_eq!(test_variable.get_content(), &[1.0, 1.0]);
+        test_variable.set_content(2.0, 3.0);
+        assert_eq!(test_variable.get_content(), &[2.0, 3.0]);
+    }
+}
--- src/factorgraph/variable/mod.rs
+++ src/factorgraph/variable/mod.rs
@@ -4,17 +4,16 @@ use std::fmt;
 pub mod pose_variable_2d;
 pub mod landmark_variable_2d;
 
+#[derive(Debug, PartialEq)]
 pub enum VariableType {
     /// position and rotation of the vehicle
     Pose2D,
-    /// position and rotation of a landmark
+    /// position and rotation of a landmark TODO change to position of a landmark OR include phi in Landmark2D and ObservationFactor2D
     Landmark2D,
 }
 
 pub trait Variable<'a>: fmt::Debug {
     fn get_id(&self) -> Uuid;
     fn get_type(&self) -> VariableType;
-
-    // TODO design interface for content
-    fn get_content(&self);
+    fn get_content(&self) -> Vec<f64>;
 }
