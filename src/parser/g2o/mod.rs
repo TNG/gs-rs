99a1ef329477eb0e880a89b72fab8584399eef8e
Samuel <samuel.valenzuela@tngtech.com>
2020-04-01 18:31:21 +0200

    Unfinished conversion of json to g2o strings

--- src/parser/g2o/mod.rs
+++ src/parser/g2o/mod.rs
@@ -15,17 +15,20 @@ pub struct G2oParser;
 
 impl Parser for G2oParser {
     fn parse_string_to_model(s: &str) -> Result<FactorGraphModel, String> {
-        JsonParser::parse_string_to_model(&G2oParser::g2o_to_json(s))
+        JsonParser::parse_string_to_model(&Self::g2o_to_json(s))
     }
 
     fn compose_model_to_string(model: FactorGraphModel) -> Result<String, String> {
-        unimplemented!()
+        match JsonParser::compose_model_to_string(model) {
+            Ok(s) => Ok(Self::json_to_g2o(&s)),
+            Err(s) => Err(s),
+        }
     }
 }
 
 impl G2oParser {
-    fn g2o_to_json(s: &str) -> String {
-        let mut json_string = ["{\n  \"vertices\":\n  [\n", s, "  ]\n}"].concat();
+    fn g2o_to_json(g2o_string: &str) -> String {
+        let mut json_string = ["{\n  \"vertices\":\n  [\n", g2o_string, "  ]\n}"].concat();
 
         let fix_regex = Regex::new("FIX (.*)\n").unwrap();
         json_string = fix_regex.replace(&json_string, "").to_string();
@@ -45,6 +48,25 @@ impl G2oParser {
         json_string = end_regex.replace(&json_string, "}\n  ]\n}").to_string();
 
         json_string
+    }
+
+    fn json_to_g2o(json_string: &str) -> String {
+        let mut g2o_string = json_string.to_owned();
+
+        let start_end_regex = Regex::new("\\{\n  \"vertices\": \\[\n((\\S|\\s)*)\n  ]\n}").unwrap();
+        g2o_string = start_end_regex.replace(&g2o_string, "$1").to_string();
+
+        let division_regex = Regex::new("  ],\n  \"edges\": \\[\n").unwrap();
+        g2o_string = division_regex.replace(&g2o_string, "").to_string();
+
+        let pose_var2d_regex = Regex::new(" {4}\\{\n {6}\"id\": (.*),\n {6}\"type\": \"POSE2D_ANGLE\",\n {6}\"position\": \\[\n {8}(.*),\n {8}(.*)\n {6}],\n {6}\"rotation\": \\[\n {8}(.*)\n {6}]\n {4}},?").unwrap();
+        g2o_string = pose_var2d_regex.replace_all(&g2o_string, "VERTEX_SE2 $1 $2 $3 $4").to_string();
+
+        // TODO finish regex replacement
+        let odometry_factor2d_regex = Regex::new(" {4}\\{").unwrap();
+        g2o_string = odometry_factor2d_regex.replace_all(&g2o_string, "EDGE_SE2 ... TODO").to_string();
+
+        g2o_string
     }
 }
 
@@ -86,4 +108,20 @@ mod tests {
             };
         dbg!("{:?}", &parsed_factor_graph);
     }
+
+    #[test]
+    fn test_dumb_json_to_g2o() {
+        init();
+
+        let file_path = "test_files/dumb.json";
+        let json_string = match fs::read_to_string(file_path) {
+            Ok(s) => s,
+            Err(_e) => panic!(format!("File could not be parsed: {}", file_path)),
+        };
+        // info!("\n{}", &json_string);
+        let g2o_string = G2oParser::json_to_g2o(&json_string);
+        info!("\n{}", &g2o_string);
+    }
+
+    // TODO Should compose_model_to_string() be tested with unit tests?
 }
