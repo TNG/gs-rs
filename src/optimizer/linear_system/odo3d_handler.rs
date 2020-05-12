b06327503ebd9a49f7495f131955177c78c4475c
Samuel <samuel.valenzuela@tngtech.com>
2020-05-12 13:01:04 +0200

    Small changes to implementation and calls of skew functions

--- src/optimizer/linear_system/odo3d_handler.rs
+++ src/optimizer/linear_system/odo3d_handler.rs
@@ -52,12 +52,16 @@ fn calc_jacobians(iso_i: &Isometry3<f64>, iso_j: &Isometry3<f64>, iso_ij: &Isome
     let mut jacobian_j = Matrix6::from_vec(vec![0.0; 36]);
     jacobian_i.index_mut((..3, ..3)).copy_from(&(-1.0 * A_rot.matrix()));
     jacobian_j.index_mut((..3, ..3)).copy_from(Err_rot.matrix());
-    jacobian_i.index_mut((..3, 3..)).copy_from(&(A_rot.matrix() * skew_trans(&B_ij.translation).transpose())); // TODO negate skew_trans? (g2o output is the exact negative) -> remove .transpose() (= *1.0 as skew-symmetric and diagonal = [0])
-    jacobian_i.index_mut((3.., 3..)).copy_from(&(dq_dR * skew_matr_T_and_mult_parts(&B_rot.matrix(), &A_rot.matrix())));
-    jacobian_j.index_mut((3.., 3..)).copy_from(&(dq_dR * skew_matr_and_mult_parts(&Matrix3::<f64>::identity(), &Err_rot.matrix())));
 
-    print!("Odometry Jacobian_i:{}", &jacobian_i);
-    print!("Odometry Jacobian_j:{}", &jacobian_j);
+    // comparing to g2o, the skew functions used in the following snippet return a transposed result
+    jacobian_i.index_mut((..3, 3..)).copy_from(&(A_rot.matrix() * skew_trans(&B_ij.translation)));
+    jacobian_i.index_mut((3.., 3..)).copy_from(&(dq_dR * skew_matr_and_mult_parts(&B_rot.matrix(), &A_rot.matrix())));
+    jacobian_j.index_mut((3.., 3..)).copy_from(&(dq_dR * skew_matr_T_and_mult_parts(&Matrix3::<f64>::identity(), &Err_rot.matrix())));
+
+    print!("ret:{}", dq_dR); // ALMOST CORRECT
+
+    print!("Odometry Jacobian_i:{}", &jacobian_i); // top left: CORRECT | top right: CORRECT | bottom left: CORRECT | bottom right: SLIGHTLY INCORRECT
+    print!("Odometry Jacobian_j:{}", &jacobian_j); // top left: ALMOST CORRECT | top right: CORRECT | bottom left: CORRECT | bottom right: ALMOST CORRECT
 
     let mut jacobian = MatrixMN::<f64, U6, U12>::from_vec(vec![0.0; 72]);
     jacobian.index_mut((.., ..6)).copy_from(&jacobian_i);
@@ -86,41 +90,41 @@ fn calc_dq_dR(matr: &Matrix3<f64>) -> MatrixMN<f64, U3, U9> {
                                             a1,  a2,  a3,])
 }
 
-// TODO remove commented out calc_dq_dR if it stays unused
-// // formula implemented according to Chapter 10.3.2 in
-// // https://jinyongjeong.github.io/Download/SE3/jlblanco2010geometry3d_techrep.pdf
-// //
-// // However: g2o passes the Error rotation matrix which always has 1s in the diagonal
-// //          (at least in the test output). This would mean that (trace - 1.0) / 2.0
-// //          would always equal 1 and always the special case would have to be called,
-// //          which does not equal the test output of g2o.
-// //
-// // TODO rename to ln_R_derivative_wrt_R() once everything is correct
-// fn calc_dq_dR(matr: &Matrix3<f64>) -> MatrixMN<f64, U3, U9> {
-//     let m = matr;
-//     let trace = get(m,0,0) + get(m,1,1) + get(m,2,2);
-//     let cos = (trace - 1.0) / 2.0;
-//     // TODO return special case if cos > 0.999999...
-//     if cos > 0.999999 {
-//         println!("special case with cos: {}", cos);
-//     }
-//     let sin = (1.0 - cos*cos).sqrt();
-//     let angle = sin.asin(); // TODO correct angle?
-//     let factor = (angle*cos - sin) / (4.0*sin*sin*sin);
-//     let a1 = (get(m,2,1) - get(m,1,2)) * factor;
-//     let a2 = (get(m,0,2) - get(m,2,0)) * factor;
-//     let a3 = (get(m,1,0) - get(m,0,1)) * factor;
-//     let b = angle / (2.0*sin);
-//     MatrixMN::<f64, U3, U9>::from_vec(vec![ a1,  a2,  a3,   // transposed matrix is displayed
-//                                            0.0, 0.0,   b,
-//                                            0.0,  -b, 0.0,
-//                                            0.0, 0.0,  -b,
-//                                             a1,  a2,  a3,
-//                                              b, 0.0, 0.0,
-//                                            0.0,   b, 0.0,
-//                                             -b, 0.0, 0.0,
-//                                             a1,  a2,  a3,])
-// }
+/*// TODO remove commented out calc_dq_dR if it stays unused
+// formula implemented according to Chapter 10.3.2 in
+// https://jinyongjeong.github.io/Download/SE3/jlblanco2010geometry3d_techrep.pdf
+//
+// However: g2o passes the Error rotation matrix which always has 1s in the diagonal
+//          (at least in the test output). This would mean that (trace - 1.0) / 2.0
+//          would always equal 1 and always the special case would have to be called,
+//          which does not equal the test output of g2o.
+//
+// TODO rename to ln_R_derivative_wrt_R() once everything is correct
+fn calc_dq_dR(matr: &Matrix3<f64>) -> MatrixMN<f64, U3, U9> {
+    let m = matr;
+    let trace = get(m,0,0) + get(m,1,1) + get(m,2,2);
+    let cos = (trace - 1.0) / 2.0;
+    // TODO return special case if cos > 0.999999...
+    if cos > 0.999999 {
+        println!("special case with cos: {}", cos);
+    }
+    let sin = (1.0 - cos*cos).sqrt();
+    let angle = sin.asin(); // TODO correct angle?
+    let factor = (angle*cos - sin) / (4.0*sin*sin*sin);
+    let a1 = (get(m,2,1) - get(m,1,2)) * factor;
+    let a2 = (get(m,0,2) - get(m,2,0)) * factor;
+    let a3 = (get(m,1,0) - get(m,0,1)) * factor;
+    let b = angle / (2.0*sin);
+    MatrixMN::<f64, U3, U9>::from_vec(vec![ a1,  a2,  a3,   // transposed matrix is displayed
+                                           0.0, 0.0,   b,
+                                           0.0,  -b, 0.0,
+                                           0.0, 0.0,  -b,
+                                            a1,  a2,  a3,
+                                             b, 0.0, 0.0,
+                                           0.0,   b, 0.0,
+                                            -b, 0.0, 0.0,
+                                            a1,  a2,  a3,])
+}*/
 
 fn skew_trans(trans: &Translation3<f64>) -> Matrix3<f64> {
     let t = 2.0 * trans.vector;
@@ -141,30 +145,40 @@ fn skew_matr_and_mult_parts(matr: &Matrix3<f64>, mult: &Matrix3<f64>) -> MatrixM
     let matr_z = mult * Matrix3::from_vec(vec![-get(m,1,0), -get(m,1,1), -get(m,1,2),    // transposed matrix is displayed
                                                 get(m,0,0),  get(m,0,1),  get(m,0,2),
                                                        0.0,         0.0,         0.0,]);
-    let mut ret = MatrixMN::<f64, U9, U3>::from_vec(vec![0.0; 27]);
-    ret.index_mut((0..3, ..)).copy_from(&matr_x);
-    ret.index_mut((3..6, ..)).copy_from(&matr_y);
-    ret.index_mut((6..9, ..)).copy_from(&matr_z);
-    ret
+    // let mut ret = MatrixMN::<f64, U9, U3>::from_vec(vec![0.0; 27]);
+    // ret.index_mut((0..3, ..)).copy_from(&matr_x);
+    // ret.index_mut((3..6, ..)).copy_from(&matr_y);
+    // ret.index_mut((6..9, ..)).copy_from(&matr_z);
+    // ret
+    let mut ret_vec: Vec<f64> = vec![];
+    ret_vec.extend_from_slice(matr_x.data.as_slice());
+    ret_vec.extend_from_slice(matr_y.data.as_slice());
+    ret_vec.extend_from_slice(matr_z.data.as_slice());
+    MatrixMN::<f64, U9, U3>::from_vec(ret_vec)
 }
 
 // TODO more elegant solution if this works
 fn skew_matr_T_and_mult_parts(matr: &Matrix3<f64>, mult: &Matrix3<f64>) -> MatrixMN<f64, U9, U3> {
     let m = &(2.0 * matr);
     let matr_x = mult * Matrix3::from_vec(vec![        0.0,         0.0,         0.0,    // transposed matrix is displayed
-                                               -get(m,2,0), -get(m,2,1), -get(m,2,2),
-                                                get(m,1,0),  get(m,1,1),  get(m,1,2),]).transpose();
-    let matr_y = mult * Matrix3::from_vec(vec![ get(m,2,0),  get(m,2,1),  get(m,2,2),    // transposed matrix is displayed
+                                                get(m,2,0),  get(m,2,1),  get(m,2,2),
+                                               -get(m,1,0), -get(m,1,1), -get(m,1,2),]);
+    let matr_y = mult * Matrix3::from_vec(vec![-get(m,2,0), -get(m,2,1), -get(m,2,2),    // transposed matrix is displayed
                                                        0.0,         0.0,         0.0,
-                                               -get(m,0,0), -get(m,0,1), -get(m,0,2),]).transpose();
-    let matr_z = mult * Matrix3::from_vec(vec![-get(m,1,0), -get(m,1,1), -get(m,1,2),    // transposed matrix is displayed
-                                                get(m,0,0),  get(m,0,1),  get(m,0,2),
-                                                       0.0,         0.0,         0.0,]).transpose();
-    let mut ret = MatrixMN::<f64, U9, U3>::from_vec(vec![0.0; 27]);
-    ret.index_mut((0..3, ..)).copy_from(&matr_x);
-    ret.index_mut((3..6, ..)).copy_from(&matr_y);
-    ret.index_mut((6..9, ..)).copy_from(&matr_z);
-    ret
+                                                get(m,0,0),  get(m,0,1),  get(m,0,2),]);
+    let matr_z = mult * Matrix3::from_vec(vec![ get(m,1,0),  get(m,1,1),  get(m,1,2),    // transposed matrix is displayed
+                                               -get(m,0,0), -get(m,0,1), -get(m,0,2),
+                                                       0.0,         0.0,         0.0,]);
+    // let mut ret = MatrixMN::<f64, U9, U3>::from_vec(vec![0.0; 27]);
+    // ret.index_mut((0..3, ..)).copy_from(&matr_x);
+    // ret.index_mut((3..6, ..)).copy_from(&matr_y);
+    // ret.index_mut((6..9, ..)).copy_from(&matr_z);
+    // ret
+    let mut ret_vec: Vec<f64> = vec![];
+    ret_vec.extend_from_slice(matr_x.data.as_slice());
+    ret_vec.extend_from_slice(matr_y.data.as_slice());
+    ret_vec.extend_from_slice(matr_z.data.as_slice());
+    MatrixMN::<f64, U9, U3>::from_vec(ret_vec)
 }
 
 fn get_isometry(pose: &[f64]) -> Isometry3<f64> {
