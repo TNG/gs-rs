236194f796e2672f6fe3bc618793f9fecc1f53f3
Samuel <samuel.valenzuela@tngtech.com>
2020-06-23 16:08:19 +0200

    Add 3D benchmarks

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -10,16 +10,24 @@ fn bench_optimization(file_name: &str, iterations: usize) {
 }
 
 fn bench_mit_2d_1(c: &mut Criterion) {
-    c.bench_function("MIT_2D", |b| b.iter(|| bench_optimization("MIT_2D", 1)));
+    c.bench_function("MIT_2D_1_iteration", |b| b.iter(|| bench_optimization("MIT_2D", 1)));
 }
 
 fn bench_mit_2d_50(c: &mut Criterion) {
-    c.bench_function("MIT_2D", |b| b.iter(|| bench_optimization("MIT_2D", 50)));
+    c.bench_function("MIT_2D_50_iterations", |b| b.iter(|| bench_optimization("MIT_2D", 50)));
+}
+
+fn bench_sphere_3d_1(c: &mut Criterion) {
+    c.bench_function("Sphere_3D_1_iteration", |b| b.iter(|| bench_optimization("Sphere_3D", 1)));
+}
+
+fn bench_sphere_3d_10(c: &mut Criterion) {
+    c.bench_function("Sphere_3D_10_iterations", |b| b.iter(|| bench_optimization("Sphere_3D", 10)));
 }
 
 criterion_group!{
     name = benches;
     config = Criterion::default().sample_size(10)/*.measurement_time(Duration::from_secs(15))*/;
-    targets = bench_mit_2d_1, bench_mit_2d_50
+    targets = bench_mit_2d_1, bench_mit_2d_50, bench_sphere_3d_1, bench_sphere_3d_10
 }
 criterion_main!(benches);
