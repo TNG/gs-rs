6316c470497123fa4d540b0c6ee0df0f09c12fa8
Samuel <samuel.valenzuela@tngtech.com>
2020-07-01 13:42:27 +0200

    Fix redundancy in linear_system mod

--- src/optimizer/linear_system/mod.rs
+++ src/optimizer/linear_system/mod.rs
@@ -29,12 +29,14 @@ pub fn calculate_H_b(factor_graph: &FactorGraph) -> (DMatrix<f64>, DVector<f64>)
 
 fn update_H_b(factor_graph: &FactorGraph, H: &mut DMatrix<f64>, b: &mut DVector<f64>, edge: EdgeReference<Factor, Directed, usize>) {
     let factor = edge.weight();
+    let var_i = &factor_graph.get_var(edge.source());
+    let var_j = &factor_graph.get_var(edge.target());
     match factor.factor_type {
-        Position2D => pos2d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source())),
-        Odometry2D => odo2d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source()), &factor_graph.get_var(edge.target())),
-        Observation2D => obs2d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source()), &factor_graph.get_var(edge.target())),
-        Position3D => pos3d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source())),
-        Odometry3D => odo3d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source()), &factor_graph.get_var(edge.target())),
-        Observation3D => obs3d_handler::update_H_b(H, b, factor, &factor_graph.get_var(edge.source()), &factor_graph.get_var(edge.target())),
+        Position2D    => pos2d_handler::update_H_b(H, b, factor, var_i),
+        Odometry2D    => odo2d_handler::update_H_b(H, b, factor, var_i, var_j),
+        Observation2D => obs2d_handler::update_H_b(H, b, factor, var_i, var_j),
+        Position3D    => pos3d_handler::update_H_b(H, b, factor, var_i),
+        Odometry3D    => odo3d_handler::update_H_b(H, b, factor, var_i, var_j),
+        Observation3D => obs3d_handler::update_H_b(H, b, factor, var_i, var_j),
     };
 }
