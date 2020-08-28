5803ae1e88ae27b79d43b2afbd5256a88eb497ce
Daniel Pape <daniel.pape@tngtech.com>
2020-08-28 17:28:07 +0200

    Even Moar OSS

--- src/factor_graph/mod.rs
+++ src/factor_graph/mod.rs
@@ -1,16 +1,16 @@
-// gs-rs - Graph SLAM in Rust
-// --------------
+// -----------------------------------------------------------------------------------------------------
+//                                      gs-rs - Graph SLAM in Rust
+// -----------------------------------------------------------------------------------------------------
 //
-// © 2020 Samuel Valenzuela (samuel.valenzuela@tngtech.com)
-// © 2020 Florian Rohm (florian.rohm@tngtech.com)
-// © 2020 Daniel Pape (daniel.pape@tngtech.com)
+// SPDX-FileCopyrightText:      © 2020 Samuel Valenzuela (samuel.valenzuela@tngtech.com)
+//                              © 2020 Florian Rohm (florian.rohm@tngtech.com)
+//                              © 2020 Daniel Pape (daniel.pape@tngtech.com)
+// SPDX-License-Identifier:     MIT OR Apache-2.0
 //
-// This product includes software developed at
-// TNG Technology Consulting GmbH (https://www.tngtech.com/).
+// This product includes software developed at TNG Technology Consulting GmbH (https://www.tngtech.com/).
 //
-// gs-rs is licensed under the Apache License, Version 2.0 (LICENSE-APACHE.md or
-// http://www.apache.org/licenses/LICENSE-2.0) or the MIT license (LICENSE-MIT.md
-// or http://opensource.org/licenses/MIT), at your option.
+
+
 //! The internal representation of a factor graph.
 
 use petgraph::csr::{Csr, NodeIndex};
