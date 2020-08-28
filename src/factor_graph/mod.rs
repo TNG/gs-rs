db8e1ad8d16ace3759cf21bb49184bfc0513ff07
Florian Rohm <rohm.florian@googlemail.com>
2020-08-28 14:51:41 +0200

    remove in code comments

--- src/factor_graph/mod.rs
+++ src/factor_graph/mod.rs
@@ -1,5 +1,4 @@
 //! The internal representation of a factor graph.
-// I have never seen a '//!' in rust, is this some special syntax?
 
 use petgraph::csr::{Csr, NodeIndex};
 use petgraph::Directed;
@@ -9,15 +8,8 @@ use std::ops::Index;
 pub mod factor;
 pub mod variable;
 
-// I would not construct the path through the crate root to increase modularity and readability (when someone does not know the crate too well one might get confused)
-// (path from crate root should not matter, when one needs something from a sibling crate I would at least question the design)
-// fashion one: use self::
-use self::factor::Factor;
-// fashion two: no self, works only in 2018 editon
+use factor::Factor;
 use variable::Variable;
-// personal taste: I would not use self but pack the relative use statements under the mod section
-
-// reference: https://doc.rust-lang.org/reference/items/use-declarations.html
 
 /// A CSR (compressed sparse row) representation of a factor graph.
 pub type FactorGraphCsr<'a> = Csr<Variable, Factor, Directed, usize>;
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,3 @@
-// as of rust 2018 edition, extern crate is not needed in most cases.
-// one special case is in here with the log and approx crates:
-// as far as I understand, the macro_use imports all macros declared with #[macro_export] in all descending modules
-// with  ```use log::info``` it is only imported in the current module and hence encapsulated.
-// one caveat: if one wants to exchange the logging macro from log::info to let's say fancy_log::info one would need to go
-// in every file and change it. Even on midsized projects this search and replace would be worth it imho.
-// For really big changes one could think of an adapter module, encapsulating this external dependency (never needed this, yet...)
-
-
 use crate::parser::g2o::G2oParser;
 use crate::parser::Parser;
 
@@ -15,8 +6,6 @@ pub mod optimizer;
 pub mod parser;
 pub mod visualizer;
 
-// Already in the Readme, I would not include this here. Or is this the intended way of calling this library?
-// Also: There is an optimize function in optimizer::optimize, this may lead to confusion.
 /// Example for the usage of the library
 pub fn optimize(in_file: &str, out_file: &str, iterations: usize) {
     let factor_graph = G2oParser::parse_file(in_file).unwrap();
